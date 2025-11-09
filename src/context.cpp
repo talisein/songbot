4b8bd2800d784d92a7b0462f2f5bcf8ec72ef2b0
Andrew Potter <agpotter@gmail.com>
2025-11-09 13:06:50 -0800

    Revert "Featured minor roles and emojis"
    This reverts commit f3a17be3f637e8bb798fad1d1a04ea608dbbd72e.

--- src/context.cpp
+++ src/context.cpp
@@ -162,6 +162,7 @@ context::setup_bot()
     bot->on_log(cerr_logger{});
 
     commands.emplace("setlist", std::make_unique<setlist_command>(*this));
+//    commands.emplace("song", std::make_unique<song_command>(*this));
     commands.emplace("last", std::make_unique<last_command>(*this));
     commands.emplace("setlistlast", std::make_unique<setlistlast_command>(*this));
 
